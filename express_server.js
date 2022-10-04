64aeb70ae39488719e6ff7155a1a5e4e0d06dc95
nicohsfu <marcnicohernandez@gmail.com>
2022-10-03 18:03:16 -0700

    added GET and POST routes for a new longURL input

--- express_server.js
+++ express_server.js
@@ -3,9 +3,23 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let strOutput = "";
+  let lettersAndNumArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for (let i = 0; i < 6; i++) {
+    let randomLetter = lettersAndNumArr[Math.floor(Math.random() * 35)];
+    strOutput += randomLetter;
+  }
+
+  return strOutput;
+}
+
 app.set("view engine", "ejs");
 
 app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +35,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/* <- What goes here? */ };
   res.render("urls_show", templateVars);
@@ -33,6 +51,12 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  // generateRandomString goes here
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
